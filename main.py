64d8b9579196116026d366d02304501810e072e8
gabholli <94938857+gabholli@users.noreply.github.com>
2022-10-16 20:04:12 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+# This is a sample Python script.
+import string
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def sportsTriviaQuestions(guess, answer):
+    global score
+    isGuessing = True
+    while isGuessing:
+        if guess == answer:
+            print("Correct Answer")
+            score += 1
+            isGuessing = False
+        else:
+            print("Incorrect Answer!")
+            break
+    print("Your score currently is: " + str(score))
+
+def inputPrompt():
+    answer = 0
+    try:
+        answer = int(input("Please select an answer: "))
+    except ValueError:
+        pass
+    return answer
+
+def multipleChoices(question, a, b, c, d):
+    print(question)
+    print("1: " + a)
+    print("2: " + b)
+    print("3: " + c)
+    print("4: " + d)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    score = 0
+    print("Welcome To The Sports Trivia Game!")
+    print("----------------------------------")
+
+    print("")
+
+
+    multipleChoices("What number equals one?", "One", "Two", "Three", "Four")
+    promptValue = inputPrompt()
+    sportsTriviaQuestions(promptValue, 1)
+    multipleChoices("What number equals two?", "One", "Two", "Three", "Four")
+    promptValueTwo = inputPrompt()
+    sportsTriviaQuestions(promptValueTwo, 2)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
